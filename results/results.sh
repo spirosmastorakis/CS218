4467596cd4284325a75d653fed09167c3084642c
spirosmastorakis <mastorakis@cs.ucla.edu>
2014-12-03 20:53:23 -0800

    results.sh: Bash script to calculate the results automatically.

---
+++ results/results.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/bash         
+
+requests=0
+delivered=0
+delay=0
+average_delay=0
+interval=0
+temp_num=0
+temp_sum=0
+
+filename="$1"
+
+while read line;
+do 
+	if [[ "$interval" == "1" ]]
+	then
+		temp_num=$(echo "scale=3; $line" | bc)
+		temp_sum=$(echo "scale=3; $temp_num + $temp_sum" | bc)
+		interval=0
+	fi	
+	if [[ "$line" == *"requests content:"* ]]
+	then
+		requests=$((requests+1))
+	fi
+    	if [[ "$line" == "SUCCESS FIRST"* ]]
+    	then
+		delivered=$((delivered+1))
+	fi
+    	if [[ $line == "Time now"* ]] 
+	then
+		interval=1
+	fi
+			
+done < $1
+
+average_delay=$(echo "scale=3; $temp_sum / $delivered" | bc)
+delivery_ratio=$(echo "scale=3; $delivered / $requests * 100" | bc)
+echo "Requests = " ${requests} >> txt.txt
+echo "Delivered = " ${delivered} >> txt.txt
+echo "Delivery ratio = " ${delivery_ratio} " %" >> txt.txt
+echo "Average delay = " ${average_delay} " sec" >> txt.txt
