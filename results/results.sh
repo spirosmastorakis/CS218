280b94a8bdd3d1b0fbc012701cef648227954794
spirosmastorakis <mastorakis@cs.ucla.edu>
2014-12-05 20:34:56 -0800

    results.sh: Compute the metrics in the right way.

--- results/results.sh
+++ results/results.sh
@@ -6,24 +6,24 @@ delay=0
 average_delay=0
 interval=0
 temp_sum=0
+temp_num=0
 cost=0
-done_delay=0
+average_cost=0
 
 filename="$1"
 
 while read line;
 do 
-	if [[ "$interval" == "1" ]]  && [[ "$done_delay" == "0" ]]
+	if [[ "$interval" == "1" ]]
 	then
-		temp_sum=$(echo "scale=3; $line" | bc)
-		#temp_sum=$(echo "scale=3; $temp_num + $temp_sum" | bc)
+		temp_num=$(echo "scale=3; $line - 0.5" | bc)
+		temp_sum=$(echo "scale=3; $temp_num + $temp_sum" | bc)
 		interval=0
-		done_delay=1
 		read line1
 		next_line=$((line1))
 		read line2
 		after_next_line=$((line2))
-		cost=$((next_line+after_next_line))
+		cost=$((next_line+after_next_line+cost))
 	fi	
 	if [[ "$line" == *"requests content:"* ]]
 	then
@@ -40,10 +40,11 @@ do
 	fi		
 done < $1
 
-average_delay=$(echo "scale=3; $temp_sum - 0.5" | bc)
+average_delay=$(echo "scale=3; ($temp_sum / $delivered)" | bc)
 delivery_ratio=$(echo "scale=3; $delivered / $requests * 100" | bc)
+average_cost=$(echo "scale=3; $cost / $delivered" | bc)
 echo "Requests = " ${requests} > txt.txt
 echo "Delivered = " ${delivered} >> txt.txt
 echo "Delivery ratio = " ${delivery_ratio} " %" >> txt.txt
 echo "Average delay = " ${average_delay} " sec" >> txt.txt
-echo "Cost = " ${cost} " packets" >> txt.txt
+echo "Average Cost = " ${average_cost} " packets" >> txt.txt
